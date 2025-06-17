6c0ec93d74e7ed2f2768321fc1ac60c246f1fb93
Uriel C. <urielchoen008@gmail.com>
2025-06-17 17:13:07 +0300

    added dockerfile

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ app = FastAPI()
 
 @app.get("/", response_class=PlainTextResponse)
 async def get_ip(request: Request):
+    # Incase we are sitting behind a proxy such as nginx-ingress
     x_forwarded_for = request.headers.get("x-forwarded-for")
     if x_forwarded_for:
         ip = x_forwarded_for.split(",")[0]
